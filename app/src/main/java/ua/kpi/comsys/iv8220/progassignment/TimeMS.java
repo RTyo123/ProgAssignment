2c63967fac877f927de6175cdf73297a257049d3
RTyo123 <warface123roadster@gmail.com>
2021-03-14 15:22:22 +0200

    Lab 1.2. Made second part of first laboratory.

---
+++ app/src/main/java/ua/kpi/comsys/iv8220/progassignment/TimeMS.java
@@ -0,0 +1,90 @@
+package ua.kpi.comsys.iv8220.progassignment;
+
+import android.annotation.SuppressLint;
+
+import java.util.Date;
+
+public class TimeMS {
+    private int hour;
+    private int minute;
+    private int seconds;
+
+    public TimeMS() {
+        this.hour = 0;
+        this.minute = 0;
+        this.seconds = 0;
+    }
+
+    public TimeMS(int hour, int minute, int seconds) {
+        if (hour >= 0 && hour <= 24)
+            this.hour = hour;
+        else
+            this.hour = 0;
+
+        if (minute >= 0 && minute <= 59)
+            this.minute = minute;
+        else
+            this.minute = 0;
+
+        if (seconds >= 0 && seconds <= 59)
+            this.seconds = seconds;
+        else
+            this.seconds = 0;
+    }
+
+    public TimeMS(Date date) {
+        hour = date.getHours();
+        minute = date.getMinutes();
+        seconds = date.getSeconds();
+    }
+
+    @SuppressLint("DefaultLocale")
+    public String getTime() {
+        return String.format("%d:%d:%d %sM", hour>12? hour-12: hour, minute, seconds, hour>12? "P": "A");
+    }
+
+    private int getTimeInSec(){
+        return hour*3600 + minute*60 + seconds;
+    }
+
+    private int[] getTimeFromSec(int secs) {
+        int newHour = secs/3600;
+        secs %= 3600;
+        int newMin = secs/60;
+        secs %= 60;
+        return new int[]{newHour, newMin, secs};
+    }
+
+    public TimeMS getTimeSum(TimeMS a, TimeMS b) {
+        int[] sum = getTimeFromSec(a.getTimeInSec() + b.getTimeInSec());
+        sum[0] %= 24;
+        return new TimeMS(sum[0], sum[1], sum[2]);
+    }
+
+    public TimeMS getTimeSum(TimeMS b) {
+        return getTimeSum(this, b);
+    }
+
+    public TimeMS getTimeSub(TimeMS a, TimeMS b) {
+        int[] sum = getTimeFromSec((24*3600)+a.getTimeInSec() - b.getTimeInSec());
+        //System.out.println(sum[0] + " " + sum[1] + " " + sum[2]);
+        sum[0] %= 24;
+        return new TimeMS(sum[0], sum[1], sum[2]);
+    }
+
+    public TimeMS getTimeSub(TimeMS b) {
+        return getTimeSub(this, b);
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+}
